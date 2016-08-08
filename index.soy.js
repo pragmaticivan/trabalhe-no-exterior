b33cd807940c22bb3bf3ed4d0d6e85ce7b9de703
Ivan Santos <pragmaticivan@gmail.com>
2016-08-07 23:14:52 -0700

    Update 2016-08-08T06:14:50.449Z

---
+++ index.soy.js
@@ -0,0 +1,23 @@
+// This file was automatically generated from index.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace index.
+ */
+
+if (typeof index == 'undefined') { var index = {}; }
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @param {Object.<string, *>=} opt_ijData
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+index.soyweb = function(opt_data, opt_ignored, opt_ijData) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml(main.layout({content: soydata.VERY_UNSAFE.$$ordainSanitizedHtmlForInternalBlocks('<header class="header"><div class="container header-wrapper"><div class="row"><div class="col-md-8"><h1 class="heading-highlight">Trabalhe no Exterior</h1></div><div class="col-md-4">' + soy.$$escapeHtml(main.topbar(null, null, opt_ijData)) + '</div></div></div></header><main>Ivan Test 3</main>')}, null, opt_ijData));
+};
+if (goog.DEBUG) {
+  index.soyweb.soyTemplateName = 'index.soyweb';
+}
